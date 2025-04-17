06f74db1b4a8f2ae75c538c0ca217de49ce48c3b
sicelo <124036348+kksambo@users.noreply.github.com>
2025-04-17 16:38:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+
+app.MapGet("/", () =>{
+
+    return "hello whats first";
+});
+
+
+app.MapPost("/send-whatsapp", async ([FromBody] WhatsAppRequest request) =>
+{
+    try
+    {
+        string accountSid = "";
+        string authToken = "";
+        string fromWhatsAppNumber = "whatsapp:+14155238886"; 
+        
+        Twilio.TwilioClient.Init(accountSid, authToken);
+
+        var message = await MessageResource.CreateAsync(
+            body: request.Message,
+            from: new PhoneNumber(fromWhatsAppNumber),
+            to: new PhoneNumber($"whatsapp:{request.ToPhoneNumber}")
+        );
+
+        return Results.Ok(new { Success = true, MessageSid = message.Sid });
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
+
+
+app.Run();
+
+public record WhatsAppRequest(string ToPhoneNumber, string Message);
