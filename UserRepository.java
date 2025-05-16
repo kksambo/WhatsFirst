687a2c2a166c09a36294745614eebc05237f5a86
Fortunate <fortubelogift@gmail.com>
2025-05-16 19:19:13 +0200

    Delete UserRepository.java

--- UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.whatsfirst.repository;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.whatsfirst.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-}
