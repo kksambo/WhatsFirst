bed438377cad4834fcf47a6f5a257388f73b0a95
B_Celumusa <celumusaenocentiabuthelezi@gmail.com>
2025-05-16 04:06:53 +0200

    Update Dashboard.jsx
    I removed the danger and warning info panel

--- source code/src/pages/Dashboard.jsx
+++ source code/src/pages/Dashboard.jsx
@@ -861,30 +861,7 @@ const Dashboard = () => {
                       </div>
                     </div>
                   </div>
-                  <div className="col-lg-6 mb-4">
-                    <div className="card text-white bg-info shadow">
-                      <div className="card-body">
-                        <p className="m-0">Information on our clients</p>
-                        <p className="text-white-50 small m-0">#36b9cc</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-6 mb-4">
-                    <div className="card text-white bg-warning shadow">
-                      <div className="card-body">
-                        <p className="m-0">Warning</p>
-                        <p className="text-white-50 small m-0">#f6c23e</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-6 mb-4">
-                    <div className="card text-white bg-danger shadow">
-                      <div className="card-body">
-                        <p className="m-0">Danger</p>
-                        <p className="text-white-50 small m-0">#e74a3b</p>
-                      </div>
-                    </div>
-                  </div>
+                           
                 </div>
               </div>
             </div>
